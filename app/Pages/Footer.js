d4c3a07f5cd96eb89ce15d9656d19e5d2e2eafed
abdarahman-shaheen <a.shaheen20001@gmail.com>
2023-10-07 18:48:17 +0000

    stand-cokkies

---
+++ app/Pages/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <div style={{position:"absolute",bottom:0,width:"100%",textAlign:'center'}}>
+      <div style={{ backgroundColor: '#15b981' , padding: '10px', color:'black',fontSize:'25px'}} class="w-screen">
+      @2023    </div>
+    </div>
+  );
+}
+
+export default Footer;
